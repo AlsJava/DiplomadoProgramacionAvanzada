31944fbf939f2f6ff1f249941dd59feca1d2d8e7
Aluis Marte <aluis.marte@reeftechnology.com>
2021-11-05 14:37:11 -0400

    Adicionando ejemplo con los comentarios.

---
+++ src/main/java/edu/aluismarte/diplomado/code/JavaComments.java
@@ -0,0 +1,14 @@
+package edu.aluismarte.diplomado.code;
+
+/**
+ * @author Aluis Marte on 11/5/2021.
+ */
+public class JavaComments {
+
+    public static void main(String[] args) {
+        // \u000A\u0053\u0079\u0073\u0074\u0065\u006d\u002E\u006F\u0075\u0074\u002E\u0070\u0072\u0069\u006E\u0074\u006C\u006E\u0028
+        // \u000A\u0022\u0057\u0054\u0046\u0021\u0022\u0029\u003B\u000A\u002F\u002A
+        System.out.println("Hello, world!");
+        // \u002A\u002F
+    }
+}
