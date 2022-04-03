3b7c302dd2517a2613588dc8a11ccc13c07babcc
Aluis Marte <aluismarte@gmail.com>
2022-04-03 02:58:39 -0400

    Adicionando mas elementos de testing con mock

--- src/test/java/edu/aluismarte/diplomado/week5/ExternalDependencyTest.java
+++ src/test/java/edu/aluismarte/diplomado/week5/ExternalDependencyTest.java
@@ -1,9 +1,12 @@
 package edu.aluismarte.diplomado.week5;
 
 import edu.aluismarte.diplomado.model.week5.ValidUser;
+import edu.aluismarte.diplomado.utils.EmailSender;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -20,21 +23,13 @@ class ExternalDependencyTest {
 
     @Test
     void sendBadEmailTest() {
-        ValidUser validUser = ValidUser.builder()
-                .id("1")
-                .name("Bad Email")
-                .email("hola@@")
-                .build();
+        ValidUser validUser = ValidUser.builder().id("1").name("Bad Email").email("hola@@").build();
         assertFalse(externalDependency.sendValidation(validUser));
     }
 
     @Test
     void sendGoodEmailTest() {
-        ValidUser validUser = ValidUser.builder()
-                .id("1")
-                .name("Bad Email")
-                .email("hola@mundo.com")
-                .build();
+        ValidUser validUser = ValidUser.builder().id("1").name("Bad Email").email("hola@mundo.com").build();
         assertTrue(externalDependency.sendValidation(validUser));
     }
 
@@ -50,28 +45,40 @@ class ExternalDependencyTest {
 
     @Test
     void validateNullKeyTest() {
-        ValidUser validUser = ValidUser.builder()
-                .id("1")
-                .name("Bad Email")
-                .email("hola@mundo.com")
-                .build();
+        ValidUser validUser = ValidUser.builder().id("1").name("Bad Email").email("hola@mundo.com").build();
         assertFalse(externalDependency.validateUser(validUser, null));
     }
 
     @Nested
-    class TheWay {
+    class TheWayTest {
+
+        private final MockedStatic<EmailSender> emailSenderMockedStatic = Mockito.mockStatic(EmailSender.class);
 
         @Test
         void validateKeyTest() {
+            emailSenderMockedStatic.when(() -> EmailSender.sendValidationEmail(ArgumentMatchers.anyString())).thenReturn(true);
             ExternalDependency externalDependencyMock = Mockito.spy(ExternalDependency.class); // Is spy not Mock
-            ValidUser validUser = ValidUser.builder()
-                    .id("10")
-                    .name("Good Email")
-                    .email("hola2@mundo.com")
-                    .build();
+            ValidUser validUser = ValidUser.builder().id("10").name("Good Email").email("hola2@mundo.com").build();
             assertTrue(externalDependencyMock.sendValidation(validUser));
             Mockito.when(externalDependencyMock.validateUser(ArgumentMatchers.any(), ArgumentMatchers.anyString())).thenReturn(true);
             assertTrue(externalDependencyMock.validateUser(validUser, "hola"));
+        }
+
+        @Test
+        void failEmailTest() {
+            emailSenderMockedStatic.when(() -> EmailSender.sendValidationEmail(ArgumentMatchers.anyString())).thenReturn(false);
+            assertFalse(EmailSender.sendValidationEmail(""));
+        }
+
+        @Test
+        void forceGoodEmailTest() {
+            emailSenderMockedStatic.when(() -> EmailSender.sendValidationEmail(ArgumentMatchers.anyString())).thenReturn(true);
+            assertTrue(EmailSender.sendValidationEmail(""));
+        }
+
+        @AfterEach
+        void unregister() {
+            emailSenderMockedStatic.close();
         }
     }
 }
