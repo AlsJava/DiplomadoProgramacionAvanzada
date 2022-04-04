d0f76e503157a4053c1d50ed93ac9a1fca61fb30
Aluis Marte <aluismarte@gmail.com>
2022-04-04 00:18:38 -0400

    Creando la base de los ejemplos y tareas a usar en la semana 6

---
+++ src/main/java/edu/aluismarte/diplomado/week6/FieldWithToManyCombinations.java
@@ -0,0 +1,9 @@
+package edu.aluismarte.diplomado.week6;
+
+/**
+ * @author aluis on 4/3/2022.
+ */
+public class FieldWithToManyCombinations {
+
+    // Hacer una matriz de campos donde tenga factores como: Requeridos, copian data default, Tengo pre data, etc
+}
---
+++ src/main/java/edu/aluismarte/diplomado/week6/HomeWork.java
@@ -0,0 +1,10 @@
+package edu.aluismarte.diplomado.week6;
+
+/**
+ * Convertir el ejemplo en un test dinámico, con tal de mejorar su forma de prueba si las reglas cambian
+ *
+ * @author aluis on 4/3/2022.
+ */
+public class HomeWork {
+
+}
