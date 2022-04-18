46ca8539691ff7d50d85ef714d3d6795b24f4b67
Aluis Marte <aluismarte@gmail.com>
2022-04-17 22:11:20 -0400

    Adicionando la parte de los test dinámicos.

--- src/test/java/edu/aluismarte/diplomado/week6/FieldWithToManyCombinationsTest.java
+++ src/test/java/edu/aluismarte/diplomado/week6/FieldWithToManyCombinationsTest.java
@@ -1,6 +1,8 @@
 package edu.aluismarte.diplomado.week6;
 
+import com.github.javafaker.Faker;
 import edu.aluismarte.diplomado.model.week3.LoanType;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author aluis on 4/4/2022.
+ * @see <a href="https://github.com/DiUS/java-faker">Java Faker</a>
  */
 public class FieldWithToManyCombinationsTest {
 
@@ -34,6 +37,20 @@ public class FieldWithToManyCombinationsTest {
     @Test
     void emptyTest() {
         assertEquals(BigDecimal.ZERO, fieldWithToManyCombinations.formValidation(NO_REQUIRED_FIELD, NO_COPY_FROM_DEFAULT, new BigDecimal(10000), BigDecimal.ZERO));
+    }
+
+    @Nested
+    class DynamicTest {
+
+        @Test
+        void generateSomeNamesTest() {
+            Faker faker = new Faker();
+            for (int i = 0; i < 20; i++) {
+                String name = faker.name().fullName();
+                System.out.println("Using name: " + name);
+                assertEquals("", fieldWithToManyCombinations.formValidation(NO_REQUIRED_FIELD, NO_COPY_FROM_DEFAULT, name, ""));
+            }
+        }
     }
 
 }
