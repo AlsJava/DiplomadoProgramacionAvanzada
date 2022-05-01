a5e4c29967e781f7a125d4abac7280c743b8eb90
Aluis Marte <aluismarte@gmail.com>
2022-05-01 11:36:25 -0400

    Adicionando más casos de prueba.

--- src/test/java/edu/aluismarte/diplomado/week2/DataTypeJavaTest.java
+++ src/test/java/edu/aluismarte/diplomado/week2/DataTypeJavaTest.java
@@ -2,7 +2,6 @@ package edu.aluismarte.diplomado.week2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -19,6 +18,20 @@ public class DataTypeJavaTest {
     }
 
     @Test
+    void intEqualsTest2() {
+        int a = 128;
+        int b = 128;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
+    void intEqualsTest3() {
+        int a = 128000;
+        int b = 128000;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
     void integerEqualTest() {
         Integer a = 127;
         Integer b = 127;
@@ -26,9 +39,44 @@ public class DataTypeJavaTest {
     }
 
     @Test
+    void integerEqualTest2() {
+        Integer a = 2;
+        Integer b = 2;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
+    void negativeIntegerEqualTest() {
+        Integer a = -2;
+        Integer b = -2;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
+    void negativeIntegerEqualTest2() {
+        Integer a = -127;
+        Integer b = -127;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
+    void negativeIntegerEqualTest3() {
+        Integer a = -129;
+        Integer b = -129;
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
     void integerNotEqualTest() {
         Integer a = 128;
         Integer b = 128;
-        assertFalse(a == b, "No equals, what?!");
+        assertTrue(a == b, "No equals, what?!");
+    }
+
+    @Test
+    void compareIntegerInGoodWayTest() {
+        Integer a = 128;
+        Integer b = 128;
+        assertTrue(a.equals(b), "No equals, what?!");
     }
 }
