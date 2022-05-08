ed3eab600b3a34396039c8f9ae373f67ec96670a
Aluis Marte <aluismarte@gmail.com>
2022-05-08 11:37:17 -0400

    Adicionando mas elementos al test

--- src/test/java/edu/aluismarte/diplomado/week3/Exercise1Test.java
+++ src/test/java/edu/aluismarte/diplomado/week3/Exercise1Test.java
@@ -1,11 +1,16 @@
 package edu.aluismarte.diplomado.week3;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author aluis on 5/8/2022.
  */
 class Exercise1Test {
+
+    private Exercise1 exercise1 = new Exercise1();
+
 
 
 }
