a456f05ce83d492e94363130ce4e6babd20be226
Aluis Marte <aluismarte@gmail.com>
2022-05-08 11:27:48 -0400

    Adicionando clase para los test del ejercicio.

--- src/main/java/edu/aluismarte/diplomado/week3/Exercise1.java
+++ src/main/java/edu/aluismarte/diplomado/week3/Exercise1.java
@@ -9,11 +9,12 @@ public class Exercise1 {
      * Un número es Fizz cuando es divisible entre 5
      * Un número es Buzz cuando es divisible entre 3
      * Un número es FizzBuzz cuando es divisible entre 3 y 5 al mismo tiempo
+     * Si no cumple con ninguna condición anterior se retorna el número
      *
      * @param number número a comprobar para saber si es Fizz, Buzz o FizzBuzz
      * @return Un string que es igual a Fizz, Buzz o FizzBuzz
      */
-    public String fizzbuzz(int number) {
-        return "";
+    public String fizzbuzz1(int number) {
+        return null;
     }
 }
---
+++ src/test/java/edu/aluismarte/diplomado/week3/Exercise1Test.java
@@ -0,0 +1,11 @@
+package edu.aluismarte.diplomado.week3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author aluis on 5/8/2022.
+ */
+class Exercise1Test {
+
+
+}
