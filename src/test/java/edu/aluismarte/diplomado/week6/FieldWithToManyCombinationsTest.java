2b216011347718c5c706918bb2615662753f786a
Aluis Marte <aluis.marte@reeftechnology.com>
2022-05-09 11:37:59 -0400

    Adicionando más formas de crear test dinamicos.

--- src/test/java/edu/aluismarte/diplomado/week6/FieldWithToManyCombinationsTest.java
+++ src/test/java/edu/aluismarte/diplomado/week6/FieldWithToManyCombinationsTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.util.stream.IntStream;
@@ -16,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author aluis on 4/4/2022.
+ * @see <a href="https://www.baeldung.com/parameterized-tests-junit-5">parameterized-tests-junit-5</a>
+ * @see <a href="https://www.baeldung.com/junit5-dynamic-tests">junit5-dynamic-tests</a>
  * @see <a href="https://github.com/DiUS/java-faker">Java Faker</a>
  */
 public class FieldWithToManyCombinationsTest {
@@ -46,6 +52,20 @@ public class FieldWithToManyCombinationsTest {
     @Nested
     class DynamicDataTest {
 
+        @ParameterizedTest
+        @CsvFileSource(resources = "/RandomNames.csv", numLinesToSkip = 1)
+        void randomNameValueFromFileTest(String name) {
+            System.out.println("Using name: " + name);
+            assertEquals("", fieldWithToManyCombinations.formValidation(NO_REQUIRED_FIELD, NO_COPY_FROM_DEFAULT, name, ""));
+        }
+
+        @ParameterizedTest
+        @MethodSource("edu.aluismarte.diplomado.week6.FieldWithToManyCombinationsTest#provideRandomNames") // Usando metodo externo a la clase
+        void randomNameValueTest(String name) {
+            System.out.println("Using name: " + name);
+            assertEquals("", fieldWithToManyCombinations.formValidation(NO_REQUIRED_FIELD, NO_COPY_FROM_DEFAULT, name, ""));
+        }
+
         @TestFactory
         Stream<DynamicTest> dynamicTestStream() {
             Faker faker = new Faker();
@@ -67,6 +87,11 @@ public class FieldWithToManyCombinationsTest {
                 assertEquals("", fieldWithToManyCombinations.formValidation(NO_REQUIRED_FIELD, NO_COPY_FROM_DEFAULT, name, ""));
             }
         }
+    }
+
+    static Stream<Arguments> provideRandomNames() {
+        Faker faker = new Faker();
+        return IntStream.range(0, 20).mapToObj(value -> Arguments.of(faker.name().fullName()));
     }
 
 }
