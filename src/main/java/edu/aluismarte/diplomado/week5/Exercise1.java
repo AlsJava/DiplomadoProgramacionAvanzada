56220e35be716f1a6803455eccc9745ea901e3e2
Aluis Marte <aluismarte@gmail.com>
2022-05-08 22:59:18 -0400

    Adicionando clases de test faltantes.

--- src/main/java/edu/aluismarte/diplomado/week5/Exercise1.java
+++ src/main/java/edu/aluismarte/diplomado/week5/Exercise1.java
@@ -4,4 +4,6 @@ package edu.aluismarte.diplomado.week5;
  * @author aluis on 5/8/2022.
  */
 public class Exercise1 {
+
+
 }
---
+++ src/test/java/edu/aluismarte/diplomado/week5/HomeworkTest.java
@@ -0,0 +1,12 @@
+package edu.aluismarte.diplomado.week5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author aluis on 5/8/2022.
+ */
+class HomeworkTest {
+
+    private final Homework homework = new Homework();
+
+}
