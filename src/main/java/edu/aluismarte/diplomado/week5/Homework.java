bc2a1a1ea93e63811a69cad1c6130c04378c57da
Aluis Marte <aluismarte@gmail.com>
2022-06-05 11:22:33 -0400

    Adicionando notas.

--- src/main/java/edu/aluismarte/diplomado/week5/Homework.java
+++ src/main/java/edu/aluismarte/diplomado/week5/Homework.java
@@ -7,28 +7,35 @@ import java.util.List;
 /**
  * ¿Como probar estos métodos?
  * <p>
- * Hacer los test principalmente de los métodos privados
+ * Hacer los test principalmente de los métodos privados (Se hace de forma indirecta)
+ *
+ * Arreglar la lógica de la función, no siempre lo que el cliente pide o la primera implementación funciona adecuadamente
  *
  * @author aluis on 4/3/2022.
  * @implNote Se hace de forma indirecta, no se prueba el método directamente
- * @implSpec La función no esta haciendo bien su trabajo, literalmente no puedo reemplazar la variable con un valor mio.
- * @implSpec solo estoy poniendo el nombre de la variable, lo cual esta mal, pues debe reemplazar por otro valor
+ * @implSpec La función no está haciendo bien su trabajo, literalmente no puedo reemplazar la variable con un valor mio.
+ * @implSpec solo estoy poniendo el nombre de la variable, lo cual está mal, pues debe reemplazar por otro valor
  */
 public class Homework {
 
-    public String prepareTemplate(List<VarsReplace> varsReplaces, String template) {
-        for (VarsReplace varsReplace : varsReplaces) {
-            template = replaceVar(template, varsReplace);
+    // TODO el método como tal, no reemplaza la variable por un texto custom y eso tenemos que arreglarlo
+
+    public String prepareTemplate(List<String> values, String template) {
+        for (String value : values) {
+            template = replaceVar(template, value);
         }
         return template;
     }
 
     // TODO test this method
-    private String replaceVar(String template, VarsReplace varsReplace) {
-        String var = "\\$\\{" + varsReplace.getVarReplace() + "\\}";
-        return template.replaceAll(var, varsReplace.getValue());
+    private String replaceVar(String template, String value) {
+        String var = "\\$\\{" + value + "\\}";
+        return template.replaceAll(var, value);
     }
 
+    /**
+     * Usar esta clase para solucionar el problema de la función
+     */
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
