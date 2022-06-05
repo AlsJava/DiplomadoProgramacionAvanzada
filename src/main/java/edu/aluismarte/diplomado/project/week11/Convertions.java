d078464c7c016eadd6f559f895ddd5e8d005b3bb
Aluis Marte <aluismarte@gmail.com>
2022-06-04 23:12:16 -0400

    Adicionando nota de la semana 11

---
+++ src/main/java/edu/aluismarte/diplomado/project/week11/Convertions.java
@@ -0,0 +1,9 @@
+package edu.aluismarte.diplomado.project.week11;
+
+/**
+ * Mostrar como los ENUM pueden ser maleables para la DB y los request, manteniendo el código simple y elegante
+ *
+ * @author aluis on 6/4/2022.
+ */
+public class Convertions {
+}
