651d8f6c0fde1bd38e14dc31aa3f1a95378e2c4f
Aluis Marte <aluismarte@gmail.com>
2022-06-12 10:28:08 -0400

    Subiendo pequeños cambios en la funcion de los logs.

--- src/test/java/edu/aluismarte/diplomado/week7/Exercise1Week7Test.java
+++ src/test/java/edu/aluismarte/diplomado/week7/Exercise1Week7Test.java
@@ -3,7 +3,9 @@ package edu.aluismarte.diplomado.week7;
 import org.junit.jupiter.api.Test;
 
 /**
- * El objetivo será hacer que se ejecute un log pero a su vez en el proceso se registre otra linea que parezca un log real pero que sea ilegal
+ * El objetivo será hacer que se ejecute un log pero
+ * a su vez en el proceso se registre otra línea que
+ * parezca un log real pero que sea ilegal
  *
  * @author aluis on 5/9/2022.
  */
@@ -12,10 +14,13 @@ class Exercise1Week7Test {
     private final Exercise1Week7 exercise1Week7 = new Exercise1Week7();
 
     @Test
-    void normalLog() {
+    void normalLogTest() {
         exercise1Week7.badLog("Normal Parameter");
     }
 
-    // TODO crear un registro de log fantasma
+    @Test
+    void anormalLogTest() {
+        // TODO crear un registro de log fantasma
+    }
 
 }
