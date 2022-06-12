dab51e51a18355f9cd1707dba1a9263024f253ed
Aluis Marte <aluismarte@gmail.com>
2022-06-12 11:41:28 -0400

    Haciendo ejemplos de loggers con multiples variantes de implementación.

--- src/main/java/edu/aluismarte/diplomado/week7/Exercise1Week7.java
+++ src/main/java/edu/aluismarte/diplomado/week7/Exercise1Week7.java
@@ -1,6 +1,7 @@
 package edu.aluismarte.diplomado.week7;
 
 import lombok.extern.slf4j.Slf4j;
+import org.owasp.encoder.Encode;
 
 /**
  * Verificar que sucede con los logs cuando no se validan las cosas antes de pasarlas
@@ -12,8 +13,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Exercise1Week7 {
 
+    public void worseLog(String parameter) {
+        // Forma común que usan los devs cuando no conocen la API de logs.
+        System.out.println("My parameter " + parameter);
+    }
+
     public void badLog(String parameter) {
+        // Forma común que usan los devs cuando no entienden que las variables se controlan
         log.info("My parameter {}", parameter);
-//        log.info("My parameter {}", Encode.forJava(parameter));
+    }
+
+    public void goodLog(String parameter) {
+        log.info("My parameter {}", Encode.forJava(parameter));
     }
 }
