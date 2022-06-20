7797f26198b36b0bb9f6f142ec775d1ebb8e6987
Aluis Marte <aluismarte@gmail.com>
2022-06-19 23:43:30 -0400

    Dejando claro las tareas y ejercicios de la semana 10

--- src/main/java/edu/aluismarte/diplomado/project/week10/Homework.java
+++ src/main/java/edu/aluismarte/diplomado/project/week10/Homework.java
@@ -1,10 +1,16 @@
 package edu.aluismarte.diplomado.project.week10;
 
 /**
+ * Se tendrá un proyecto por alumno donde tendrá que aplicar el patrón SAGA.
+ *
+ * Se hará el proceso de contactar la API usando la SAGA para simular procesos desconectados
+ *
+ * Crear procesos de: Crear pago, Cancelar pago, reembolso de un pago
+ *
  * @author aluis on 6/4/2022.
+ * @implSpec Ahora los elementos del command llamarán la SAGA y tendrán la lógica deseada
  */
 public class Homework {
 
-    // La tarea será aplicar este patrón al diseño
-    // Puede que se tenga que adicionar un requerimiento
+    // TODO La tarea será aplicar este patrón al diseño
 }
