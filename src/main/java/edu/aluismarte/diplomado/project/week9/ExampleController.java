a376f26fe0ef3b62f49e423299fdba55df8e70e0
Aluis Marte <aluismarte@gmail.com>
2022-06-26 12:52:59 -0400

    Adicionando ejemplo de un controller que sera la base de nuestro API y todo el proyecto.

---
+++ src/main/java/edu/aluismarte/diplomado/project/week9/ExampleController.java
@@ -0,0 +1,35 @@
+package edu.aluismarte.diplomado.project.week9;
+
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Ejemplo de como crear un controlador con sus diversos métodos
+ *
+ * @author aluis on 6/26/2022.
+ */
+@RestController
+@RequiredArgsConstructor
+public class ExampleController {
+
+    @GetMapping("/example/demo/{id}")
+    public ResponseEntity<String> demoPath(@PathVariable String id) {
+        return ResponseEntity.ok("Tengo el ID" + id);
+    }
+
+    @PostMapping("/example/demo")
+    public ResponseEntity<String> demoPost(@RequestBody DemoRequest demoRequest) {
+        return ResponseEntity.ok("Tengo el objeto: " + demoRequest);
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @ToString
+    static class DemoRequest {
+        private String id;
+        private long number;
+    }
+}
