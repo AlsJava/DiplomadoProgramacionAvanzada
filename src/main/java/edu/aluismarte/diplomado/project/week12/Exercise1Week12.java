0181ceb1214e2de1e3f05ceb18bcbcca45b5a5d8
Aluis Marte <aluismarte@gmail.com>
2022-06-26 12:46:25 -0400

    Agregando material para la semana 12

--- src/main/java/edu/aluismarte/diplomado/project/week12/Exercise1Week12.java
+++ src/main/java/edu/aluismarte/diplomado/project/week12/Exercise1Week12.java
@@ -1,9 +1,16 @@
 package edu.aluismarte.diplomado.project.week12;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
+ * Hacer una implementación del sistema de LOG correctamente
+ *
+ * Hacer que casa proceso haga el log adecuado según la necesidad
+ *
  * @author aluis on 6/4/2022.
  */
+@Slf4j
 public class Exercise1Week12 {
 
-    // TODO realidar la implementación de un log adecuado.
+    // TODO construir metodos para hacer logs logs adecuados
 }
---
+++ src/main/java/edu/aluismarte/diplomado/project/week12/ReplicateEventsInMicroServices.java
@@ -0,0 +1,11 @@
+package edu.aluismarte.diplomado.project.week12;
+
+/**
+ * A veces el DEBUG no es suficiente, necesitamos saber que ocurre con un proceso con ciertos eventos
+ *
+ * Por lo que haremos un follo que permitirá hacer nuevamente el proceso con tal de replicar paso a paso
+ *
+ * @author aluis on 6/26/2022.
+ */
+public class ReplicateEventsInMicroServices {
+}
