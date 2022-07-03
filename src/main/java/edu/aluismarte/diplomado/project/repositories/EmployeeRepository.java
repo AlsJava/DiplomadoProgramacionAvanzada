26968ec423b64a93b31c636f21da5bda0cf89111
Aluis Marte <aluismarte@gmail.com>
2022-07-03 10:24:59 -0400

    Adicionando annotations

--- src/main/java/edu/aluismarte/diplomado/project/repositories/EmployeeRepository.java
+++ src/main/java/edu/aluismarte/diplomado/project/repositories/EmployeeRepository.java
@@ -2,9 +2,11 @@ package edu.aluismarte.diplomado.project.repositories;
 
 import edu.aluismarte.diplomado.project.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author aluis on 6/12/2022.
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- src/main/java/edu/aluismarte/diplomado/project/repositories/PaymentRepository.java
+++ src/main/java/edu/aluismarte/diplomado/project/repositories/PaymentRepository.java
@@ -2,11 +2,13 @@ package edu.aluismarte.diplomado.project.repositories;
 
 import edu.aluismarte.diplomado.project.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 /**
  * @author aluis on 6/12/2022.
  */
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
